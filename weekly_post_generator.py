b5aa0163477894df6beb8ce58fb9a44f543ac8c2
ramko911 <rosecorentin@hotmail.com>
2025-07-31 00:48:49 +0200

    Add files via upload

--- weekly_post_generator.py
+++ weekly_post_generator.py
@@ -10,7 +10,7 @@ st.set_page_config(
     layout="centered"
 )
 
-st.image("./favicon.png", width=120)
+st.image("wantumeni_logo_NEG_02.png", width=120)
 st.title("🎧 Wantumeni Post Generator")
 
 # File paths
